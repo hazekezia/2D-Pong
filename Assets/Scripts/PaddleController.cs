e45afe33d8ea9afb040b961a3980a1439131b721
Jonathan Sitohang <hazekezia@gmail.com>
2022-06-08 23:01:18 +0700

    Base gameplay

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public int speed;
+    public KeyCode upKey, downKey;
+    private Rigidbody2D rig;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector2 movement;
+        movement = GetInput();
+
+        MoveObject(movement);
+    }
+
+    private Vector2 GetInput(){
+
+        //user input
+        if(Input.GetKey(upKey)){
+            //atas
+            return Vector2.up * speed;
+        }
+        else if(Input.GetKey(downKey)){
+            //bawah
+            return Vector2.down * speed;
+        }
+
+        return Vector2.zero;
+    }
+
+    private void MoveObject(Vector2 movement){
+        //gerakan object
+        rig.velocity = movement;
+    }
+}
