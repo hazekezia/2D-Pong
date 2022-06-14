63c939ceea1c295ea5fbbc886bf20fb9bb91545d
Jonathan Sitohang <hazekezia@gmail.com>
2022-06-14 23:41:06 +0700

    Adding speed up power up

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void PlayGame(){
+        Debug.Log("Jonathan Jeremia Valentino Vici Sitohang - 149251970101-26");
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OpenAuthor(){
+        Debug.Log("Created by Jonathan Jeremia Valentino Vici Sitohang");
+    }
+}
